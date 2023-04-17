52dddb808c6c2dbc4802d2dfc4dd3ca37c8da644
Hans Wang <hansmikaa@live.cn>
2023-04-16 22:05:14 -0400

    implement vertical search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
-}
+
+    // map((col, i)) is to get index i to iterative.
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+};
 
 module.exports = wordSearch
